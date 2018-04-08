7642fb01dd947d45c1a91aaff0427cc20f5076a7
Tiffany Green <tiffanydgreen@gmail.com>
2018-04-08 12:34:19 -0500

    Add conditionals for toppings and adjust base price. Use intent to output summary to email

--- JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.android.justjava;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.NumberFormat;
 
@@ -24,17 +30,49 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int price = calculatePrice();
-        displayMessage(createOrderSummary(price));
+        CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream);
+        boolean whipped_cream = whippedCreamCheckBox.isChecked();
+
+        CheckBox chocolateCheckBox = (CheckBox) findViewById(R.id.chocolate);
+        boolean chocolate = chocolateCheckBox.isChecked();
+
+        EditText nameInput = (EditText) findViewById(R.id.customer_name);
+        String nameValue = nameInput.getText().toString();
+
+        int price = calculatePrice(chocolate, whipped_cream);
+
+        emailOrderSummary(nameValue, price, whipped_cream, chocolate);
     }
 
-    private int calculatePrice() {
-        return quantity * 5;
+    private int calculatePrice(boolean chocolate, boolean whipped_cream) {
+        int base_price = 5;
+
+        if (whipped_cream == true) {
+            base_price = base_price + 1;
+        }
+
+        if (chocolate == true) {
+            base_price = base_price + 2;
+        }
+        return quantity * base_price;
     }
 
-    private String createOrderSummary(int price) {
-        String priceMessage = "Name: Kaptain Kunal\n";
-        priceMessage += "Quantity: " + quantity;
+    private void emailOrderSummary(String name, int price, boolean cream, boolean chocolate) {
+        String sendMessage = createOrderSummary(name, price, cream, chocolate);
+
+        Intent emailOrderIntent = new Intent(Intent.ACTION_SENDTO);
+        emailOrderIntent.setData(Uri.parse("mailto:"));
+        emailOrderIntent.putExtra(Intent.EXTRA_SUBJECT, "JustJava order for " + name);
+        emailOrderIntent.putExtra(Intent.EXTRA_TEXT, sendMessage);
+        if (emailOrderIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(emailOrderIntent);
+        }
+    }
+    private String createOrderSummary(String name, int price, boolean cream, boolean chocolate) {
+        String priceMessage = "Name: " + name;
+        priceMessage += "\nAdd whipped cream? " + cream;
+        priceMessage += "\nAdd chocolate topping? " + chocolate;
+        priceMessage += "\nQuantity: " + quantity;
         priceMessage += "\nTotal: $" + price;
         priceMessage += "\nThank you!";
         return priceMessage;
@@ -51,18 +89,27 @@ public class MainActivity extends AppCompatActivity {
     /**
      * This method displays the given text on the screen.
      */
-    private void displayMessage(String message) {
-        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
-        orderSummaryTextView.setText(message);
-    }
+//    private void displayMessage(String message) {
+//        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+//        orderSummaryTextView.setText(message);
+//    }
 
     public void increment(View view){
-        quantity = quantity + 1;
+        if (quantity < 100 ) {
+            quantity = quantity + 1;
+        } else {
+            Toast.makeText(this, "You cannot order more than 100 cups", Toast.LENGTH_SHORT).show();
+        }
         displayQuantity(quantity);
     }
 
     public void decrement(View view) {
-        quantity = quantity - 1;
+        if (quantity > 1) {
+            quantity = quantity - 1;
+        } else {
+            Toast.makeText(this, "You cannot order less than one cup", Toast.LENGTH_SHORT).show();
+        }
+
         displayQuantity(quantity);
     }
 }
