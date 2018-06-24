a0311eebe25e54f47391c4cae1f8697b60e89379
squareleaf <tiffanydgreen@gmail.com>
2018-06-24 17:56:25 -0500

    Switch xml onclick to OnClickListener in Java

--- RunningMusicApp/app/src/main/java/com/example/tiffany/runningmusicapp/MainActivity.java
+++ RunningMusicApp/app/src/main/java/com/example/tiffany/runningmusicapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,10 +12,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void openNowPlaying(View view) {
-        Intent playingIntent = new Intent(this, NowPlaying.class);
-        startActivity(playingIntent);
+        TextView slowestButton = (TextView) findViewById(R.id.slowestButton);
+        TextView slowButton = (TextView) findViewById(R.id.slowButton);
+        TextView fastButton = (TextView) findViewById(R.id.fastButton);
+        TextView fastestButton = (TextView) findViewById(R.id.fastestButton);
+
+        slowestButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent playingIntent = new Intent(MainActivity.this, NowPlaying.class);
+                startActivity(playingIntent);
+            }
+        });
+
+        slowButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent playingIntent = new Intent(MainActivity.this, NowPlaying.class);
+                startActivity(playingIntent);
+            }
+        });
+
+        fastButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent playingIntent = new Intent(MainActivity.this, NowPlaying.class);
+                startActivity(playingIntent);
+            }
+        });
+
+        fastestButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent playingIntent = new Intent(MainActivity.this, NowPlaying.class);
+                startActivity(playingIntent);
+            }
+        });
     }
 }
