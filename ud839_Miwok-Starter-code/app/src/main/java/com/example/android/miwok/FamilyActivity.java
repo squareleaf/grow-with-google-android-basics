bbd253aa9ae27eadddee1e499e08367e5976d33c
squareleaf <tiffanydgreen@gmail.com>
2018-07-22 21:13:48 -0500

    Add audio files and make them playable

--- ud839_Miwok-Starter-code/app/src/main/java/com/example/android/miwok/FamilyActivity.java
+++ ud839_Miwok-Starter-code/app/src/main/java/com/example/android/miwok/FamilyActivity.java
@@ -1,32 +1,45 @@
 package com.example.android.miwok;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
 public class FamilyActivity extends AppCompatActivity {
+    private MediaPlayer mMediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.word_list);
 
-        ArrayList<Word> members = new ArrayList<Word>();
-        members.add(new Word("father", "әpә", R.drawable.family_father));
-        members.add(new Word("mother", "әṭa", R.drawable.family_mother));
-        members.add(new Word("son", "angsi", R.drawable.family_son));
-        members.add(new Word("daughter", "tune", R.drawable.family_daughter));
-        members.add(new Word("older brother", "taachi", R.drawable.family_older_brother));
-        members.add(new Word("younger brother", "chalitti", R.drawable.family_younger_brother));
-        members.add(new Word("older sister", "teṭe", R.drawable.family_older_sister));
-        members.add(new Word("younger sister", "kolliti", R.drawable.family_younger_sister));
-        members.add(new Word("grandmother", "ama", R.drawable.family_grandmother));
-        members.add(new Word("grandfather", "paapa", R.drawable.family_grandfather));
+        final ArrayList<Word> members = new ArrayList<Word>();
+        members.add(new Word("father", "әpә", R.drawable.family_father, R.raw.family_father));
+        members.add(new Word("mother", "әṭa", R.drawable.family_mother, R.raw.family_mother));
+        members.add(new Word("son", "angsi", R.drawable.family_son, R.raw.family_son));
+        members.add(new Word("daughter", "tune", R.drawable.family_daughter, R.raw.family_daughter));
+        members.add(new Word("older brother", "taachi", R.drawable.family_older_brother, R.raw.family_older_brother));
+        members.add(new Word("younger brother", "chalitti", R.drawable.family_younger_brother, R.raw.family_younger_brother));
+        members.add(new Word("older sister", "teṭe", R.drawable.family_older_sister, R.raw.family_older_sister));
+        members.add(new Word("younger sister", "kolliti", R.drawable.family_younger_sister, R.raw.family_younger_sister));
+        members.add(new Word("grandmother", "ama", R.drawable.family_grandmother, R.raw.family_grandmother));
+        members.add(new Word("grandfather", "paapa", R.drawable.family_grandfather, R.raw.family_grandfather));
 
         WordAdapter familyAdapter = new WordAdapter(this, members, R.color.category_family);
         ListView listView = (ListView) findViewById(R.id.list);
         listView.setAdapter(familyAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Word member = members.get(position);
+                mMediaPlayer = MediaPlayer.create(FamilyActivity.this, member.getAudioFileId());
+                mMediaPlayer.start();
+            }
+        });
     }
 }
--- ud839_Miwok-Starter-code/app/src/main/java/com/example/android/miwok/Word.java
+++ ud839_Miwok-Starter-code/app/src/main/java/com/example/android/miwok/Word.java
@@ -6,16 +6,19 @@ public class Word {
     private String mDefaultTranslation;
     private int mImageResourceId = NO_IMAGE_PROVIDED;
     private static final int NO_IMAGE_PROVIDED = -1;
+    private int mAudioFileId;
 
-    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId) {
+    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId, int audioFileId) {
         mMiwokTranslation = miwokTranslation;
         mDefaultTranslation = defaultTranslation;
         mImageResourceId = imageResourceId;
+        mAudioFileId = audioFileId;
     }
 
-    public Word(String defaultTranslation, String miwokTranslation) {
+    public Word(String defaultTranslation, String miwokTranslation, int AudioFileId) {
         mMiwokTranslation = miwokTranslation;
         mDefaultTranslation = defaultTranslation;
+        mAudioFileId = AudioFileId;
     }
 
     public String getMiwokTranslation() {
@@ -27,6 +30,8 @@ public class Word {
     }
 
     public int getImageResourceId() { return mImageResourceId; }
+
+    public int getAudioFileId() { return mAudioFileId; }
 
     public boolean hasImage() {
         return mImageResourceId != NO_IMAGE_PROVIDED;
